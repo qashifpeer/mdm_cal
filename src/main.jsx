045ea6a6ac786c078595c3cf5c2fd6ade786bf52
Qashif Peer <76738563+qashifpeer@users.noreply.github.com>
2023-06-27 21:13:47 +0530

    MainContextError Removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import MainContextProvider from "./contexts/mainContext.jsx";
+import MainContextProvider from "./contexts/MainContext.jsx";
 import CalculateContextProvider from "./contexts/CalculateContext.jsx";
 import RiceContextProvider from "./contexts/RiceContext.jsx";
 
