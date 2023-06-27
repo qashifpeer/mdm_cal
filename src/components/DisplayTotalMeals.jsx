ed41a8b680530cbb290a938fc2fb68e9f01dbd3d
Qashif Peer <76738563+qashifpeer@users.noreply.github.com>
2023-06-27 21:17:25 +0530

    context error removed

--- src/components/DisplayTotalMeals.jsx
+++ src/components/DisplayTotalMeals.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { MainContext } from "../contexts/mainContext";
+import { MainContext } from "../contexts/MainContext";
 
 const DisplayTotalMeals = () => {
   const  {totalDays}  = useContext(MainContext);
